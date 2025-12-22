ea90db1ce51c8852cfed1e9001ce846b7582633c
Kim-jiyun318 <jiyunkim3607@gmail.com>
2025-12-22 20:56:57 +0900

    config/urls.py 수정

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
 
     # 2. dash 앱 연결 (http://127.0.0.1:8000/dash/)
     path('dash/', include('dash.urls')),
+
+    path('auth/', include('users.urls')),
     
     # (선택사항) 만약 주소창에 아무것도 안 쳤을 때(http://127.0.0.1:8000/) 
     # 바로 대시보드로 가게 하고 싶다면 아래 줄을 추가하세요.
